97db406962c054032a0e5118b0761c305d057c13
zhanghang <zhcppy@icloud.com>
2020-07-13 10:08:40 +0800

    a small step

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -16,17 +16,21 @@ impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             // Something goes here...
+            panic!("weight_in_grams can't less than zero");
+            // return Package { sender_country, recipient_country, weight_in_grams };
         } else {
-            return Package {sender_country, recipient_country, weight_in_grams};
+            return Package { sender_country, recipient_country, weight_in_grams };
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
+        return self.recipient_country == "Russia";
         // Something goes here...
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
         // Something goes here... (beware of grams to kg conversion)
+        return self.weight_in_grams + cents_per_kg;
     }
 }
 
@@ -47,7 +51,7 @@ mod tests {
     fn create_international_package() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Russia");
-        
+
         let package = Package::new(sender_country, recipient_country, 1200);
 
         assert!(package.is_international());
@@ -58,10 +62,10 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
-        
+        let cents_per_kg = 3000;
+
         let package = Package::new(sender_country, recipient_country, 1500);
-        
+
         assert_eq!(package.get_fees(cents_per_kg), 4500);
     }
 }
